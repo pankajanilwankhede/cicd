1393f08e476a90ce017adf7c87c15c53da668c78
Pankaj Anil Wankhede <pankajw032002@gmail.com>
2024-10-14 20:39:19 +0530

    test file

---
+++ test_app.py
@@ -0,0 +1,13 @@
+# test_app.py
+import pytest
+from app import app as flask_app  # Import the Flask app from app.py
+
+@pytest.fixture
+def client():
+    with flask_app.test_client() as client:
+        yield client
+
+def test_hello(client):
+    response = client.get('/')
+    assert response.data == b'Hello, CI/CD with GitHub Actions!'  # Adjust this assertion based on your app's response
+
